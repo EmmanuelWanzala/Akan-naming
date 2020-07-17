ba4f50d61d818dc824284262226972c000659c7b
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-07-17 16:34:25 +0300

    add initial commit

---
+++ css/week.js
@@ -0,0 +1,66 @@
+
+
+
+
+
+
+
+Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+
+ function  Akhan(birthday,gender){
+if(male && sunday){
+    "Kwasi")
+}
+else if(male && monday){
+    console.log("Kwadwo")
+}
+else if(male && tuesday){
+    console.log("Kwabena")
+}
+else if(male && wednesday){
+    console.log("Kwaku")
+}
+else if(male && thursday){
+    console.log("Yaw")
+}
+else if(male && friday){
+    console.log("Koffi")
+}
+else if(male && saturday){
+    console.log("Kwame")
+}
+else if(male && sunday){
+    console.log("Akosua")
+}
+else if(female && monday){
+    console.log("Adwoa")
+}
+else if(female && tuesday){
+    console.log("Abenaa")
+}
+else if(female && wednesday){
+    console.log("Akua")
+}
+else if(female && thursday){
+    console.log("Yaa")
+}
+else if(male && frday){
+    console.log("Afua")
+}
+else if(male && saturday){
+    console.log("Ama")
+}
+else{
+    console.log("Invalid data")
+}
+}
+
+var birthday=parseInt(prompt("19/98/11/23"))
+var gender=prompt("male")
+
+
+
+
+
+
+
