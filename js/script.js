45e7837e146e7bad0265d297187b1d717673c441
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-07-18 23:27:53 +0300

    added variables in js

--- js/script.js
+++ js/script.js
@@ -40,33 +40,33 @@
 
 // }
 var submission = function() { 
-    var daysOfTheWeek = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-    var maleNames = [
-      "Kwasi",
-      "Kwadwo",
-      "Kwabena",
-      "Kwaku",
-      "Yaw",
-      "Kofi",
-      "Kwame"
-    ];
-    var femaleNames = [
-      "Akosua",
-       " Adwoa",
-      "Abenaa",
-      "Akua",
-      "Yaa",
-      "Afua",
-      "Ama"
-    ];
+    var daysOfTheWeek = ["Sunday","Monday","Tuesday", "Wednesday","Thursday","Friday","Saturday"];
+    //   "Sunday",
+    //   "Monday",
+    //   "Tuesday",
+    //   "Wednesday",
+    //   "Thursday",
+    //   "Friday",
+    //   "Saturday"
+    // ];
+    var maleNames = [ "Kwasi","Kwadwo","Kwabena","Kwaku", "Yaw", "Kofi","Kwame"];
+    //   "Kwasi",
+    //   "Kwadwo",
+    //   "Kwabena",
+    //   "Kwaku",
+    //   "Yaw",
+    //   "Kofi",
+    //   "Kwame"
+    // ];
+    var femaleNames = ["Akosua"," Adwoa", "Abenaa","Akua","Yaa", "Afua", "Ama"];
+    //   "Akosua",
+    //    " Adwoa",
+    //   "Abenaa",
+    //   "Akua",
+    //   "Yaa",
+    //   "Afua",
+    //   "Ama"
+    // ];
     var month = parseInt(document.getElementById("month").value);
     var year = parseInt(document.getElementById("year").value);
     var day = parseInt(document.getElementById("day").value);
@@ -77,7 +77,7 @@ var submission = function() {
     var male = document.getElementById("male")
     var female = document.getElementById("female")
     
-    if (month =="" && year =="" && day =="" && name=="") {
+    if (month ==="" && year ==="" && day ==="" && name==="") {
       alert("Please Enter you credentials");
       return false;
     }
